232b618954c7faa674ae569cfc1d4c9e6fd851dc
Noam Lerner <noaml1295@gmail.com>
2017-03-27 17:08:27 -0400

    s

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,16 @@
 var ChessBoard = require("chessboardjs")
-board = ChessBoard('board', 'start')
+var Chess  = require('chess.js').Chess
+
+var config = {
+    draggable: true, // piece interaction
+    position: 'start', // starting configuration
+    onDrop: handleMove, // call this function
+}
+
+board = ChessBoard('board', config)
+
+game = new Chess()
+
+var handleMove = function(source, target) {
+	var move = game.move({from:source, to:target})
+}
