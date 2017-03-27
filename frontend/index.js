0f995677d548453d7a54134ce0cd19334f111879
Noam Lerner <noaml1295@gmail.com>
2017-03-27 17:34:23 -0400

    s

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,23 @@
 var ChessBoard = require("chessboardjs")
 var Chess  = require('chess.js').Chess
+var socket = require('socket.io-client')()
 
-var config = {
-    draggable: true, // piece interaction
-    position: 'start', // starting configuration
-    onDrop: handleMove, // call this function
+var initGame = function (){
+	var config = {
+	    draggable: true, // piece interaction
+	    position: 'start', // starting configuration
+	    onDrop: handleMove, // call this function
+	}
+
+	board = ChessBoard('board', config)
+
+	game = new Chess()	
 }
-
-board = ChessBoard('board', config)
-
-game = new Chess()
 
 var handleMove = function(source, target) {
 	var move = game.move({from:source, to:target})
+	socket.emit('message',source + " --> " + target)
 }
+
+
+initGame()
